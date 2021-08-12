f320530d35dad56b6e5a3101ddabdd6457b592d6
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-12 13:56:53 -0300

    Classe ServiceCliente criada, onde tem um metódo que pesquisa meu cliente pelo cpf dele.

--- src/main/java/com/br/zup/ecommerceZUP/service/ClienteService.java
+++ src/main/java/com/br/zup/ecommerceZUP/service/ClienteService.java
@@ -11,12 +11,19 @@ public class ClienteService {
     //Lista de clientes.
     private List<Cliente> clientes = new ArrayList<>();
 
-    //CadastrarCliente
+    //Cadastrar Cliente
     public Cliente cadastrarCliente(Cliente cliente){
         clientes.add(cliente);
         return cliente;
     }
 
-
-
+    //Pesquisar Cliente pelo cpf
+    public Cliente pesquisarCliente(String cpf) throws Exception {
+        for (Cliente cliente : clientes) {
+            if (cliente.getCpf().equals(cpf)) {
+                return cliente;
+            }
+        }
+        throw new Exception("Cliente não encontrado");
+    }
 }
