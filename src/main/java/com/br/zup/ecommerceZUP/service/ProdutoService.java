902a54e02013c34c50fc5fae1888d24957f34551
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-12 15:03:10 -0300

    Classe ProdutoService criada, com os metódos de cadastrar produto, verificar o produto já existente pelo nome e código e mostrar produto.

--- src/main/java/com/br/zup/ecommerceZUP/service/ProdutoService.java
+++ src/main/java/com/br/zup/ecommerceZUP/service/ProdutoService.java
@@ -1,4 +1,34 @@
 package com.br.zup.ecommerceZUP.service;
 
+import com.br.zup.ecommerceZUP.dominio.Produto;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProdutoService {
+    //Lista de Produtos
+    private List<Produto> produtos = new ArrayList<>();
+
+    //Cadastrar Produto
+    public void cadastrarProduto(Produto produto){
+        produtos.add(produto);
+    }
+    //Verificar Produto já existente
+    public void produtoDuplicado(String nome, String codigo) throws Exception {
+        for (Produto produto : produtos) {
+            if (produto.getNome().equals(nome) && produto.getCodigoDoProduto().equals(codigo)) {
+                throw new Exception("Produto já cadastrado");
+
+            }
+        }
+    }
+
+    //Mostrar Produtos
+    public List<Produto> mostrarProdutos(){
+        return produtos;
+    }
+
 }
