54d9d20d3d2ef7738d3db95f28944f20a59864b7
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-12 13:39:43 -0300

    Iniciando o desafio do modulo 4

---
+++ src/main/java/com/br/zup/ecommerceZUP/EcommerceZupApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.ecommerceZUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zup/ecommerceZUP/EcommerceZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.ecommerceZUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
